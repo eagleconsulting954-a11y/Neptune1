f5b48a494b7d5e99d63bddbb89159b513fe766de
eagleconsulting954-a11y <eagleconsulting954@gmail.com>
2026-07-10 00:54:44 -0400

    Add public site header

---
+++ components/SiteHeader.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export function SiteHeader() {
+  return (
+    <header className="site-header">
+      <div className="container header-inner">
+        <Link className="brand" href="/"><span className="brand-mark">✦</span><span>NEPTUNE<small>Vessel Command CRM</small></span></Link>
+        <nav className="nav">
+          <Link href="/#platform">Platform</Link>
+          <Link href="/#modules">Modules</Link>
+          <Link href="/resources">Resources</Link>
+          <Link href="/pricing">Pricing</Link>
+          <Link href="/login">Login</Link>
+          <Link className="btn gold" href="/signup">Start trial</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
