f95cc720f3e43f5466901edf0be38d2867e15873
eagleconsulting954-a11y <eagleconsulting954@gmail.com>
2026-08-13 21:22:26 -0400

    Remove public admin navigation link

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -12,7 +12,6 @@ export function SiteHeader() {
           <Link href="/demo">Live Demo</Link>
           <Link href="/resources">Resources</Link>
           <Link href="/pricing">Pricing</Link>
-          <Link href="/admin">Admin</Link>
           <Link href="/login">Login</Link>
           <InstallAppButton className="btn install-nav-button" label="Download app" />
           <Link className="btn gold" href="/signup">Start trial</Link>
